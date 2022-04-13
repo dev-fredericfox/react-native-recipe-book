56de3b6ae013aa3aa506194fb1d9c6fed358d749
Frederic Fox <dev.frederic.fox@gmail.com>
2022-04-13 18:26:08 +0200

    Change some spacing and layout

--- screens/RecipeScreen.tsx
+++ screens/RecipeScreen.tsx
@@ -1,38 +1,84 @@
 import React, { useEffect, useState } from "react";
-import { ActivityIndicator, FlatList, Image,SafeAreaView,ScrollView,StyleSheet,Text, View } from "react-native";
+import {
+  ActivityIndicator,
+  FlatList,
+  Image,
+  SafeAreaView,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
+import tw from "twrnc";
 
-import Device from "../constants/Device"
+import IngredientList from "../components/IngredientList";
+import Device from "../constants/Device";
 
-export default function RecipeScreen({route}) {
+export default function RecipeScreen({ route }) {
   const [isLoading, setLoading] = useState(true);
   const [data, setData] = useState([]);
-  const {title, coverimg, ingredients, category, content} = route.params
+  const [number, setNumber] = useState('1');
+
+  const { title, coverimg, ingredients, category, content } = route.params;
+
+  const renderItem = ({ item }) => (
+    <IngredientList
+      ingredient={item.ingredient}
+      emoji={item.emoji}
+      amount={item.amount}
+      unit={item.unit}
+      multiplicationFactor={number}
+    />
+  );
 
   return (
-    <SafeAreaView >
-    <ScrollView style={{ paddingLeft: 24 }}>
-      <Text>{route.params.title}</Text>
-      <Image
-        source={{
-          uri: coverimg,
-        }}
-        resizeMode="cover"
-        style={[styles.coverimg]}
-      />
-      <Text>Ingredients {ingredients.length}</Text>
-      <Text>Recipe</Text>
-      <Text>{content}</Text>
-      </ScrollView>
-    </SafeAreaView>
+    <FlatList
+      ListHeaderComponent={
+        <>
+          <Text style={tw`text-2xl font-bold text-center`}>{title}</Text>
+          <Image
+            source={{
+              uri: coverimg,
+            }}
+            resizeMode="cover"
+            style={[styles.coverimg, tw`my-6`]}
+          />
+          <Text style={tw`text-2xl font-bold`}>
+            Ingredients ({ingredients.length})
+          </Text>
+        </>
+      }
+      data={ingredients}
+      keyExtractor={({ ingredient }, index) => ingredient}
+      renderItem={renderItem}
+      style={tw`px-6`}
+      ListFooterComponent={
+        <>
+          <View style={tw`flex flex-row-reverse mt-4`}>
+          <TextInput
+              style={tw`w-1/6 bg-white text-lg rounded-lg h-10`}
+              onChangeText={setNumber}
+              value={number}
+              textAlign={'center'}
+              placeholder="1"
+              keyboardType="numeric"
+            />
+            <Text style={tw`text-lg my-auto`}>Ingredient Ratio: </Text>
+
+          </View>
+          <Text style={tw`text-2xl font-bold`}>Recipe</Text>
+          <Text>{content}</Text>
+        </>
+      }
+    />
   );
 }
 
-
 const styles = StyleSheet.create({
   coverimg: {
-  width: Device.window.width - 48,
-  borderRadius: 20,
-  height: 200,
-  }
-
-})
+    width: Device.window.width - 48,
+    borderRadius: 20,
+    height: 200,
+  },
+});
