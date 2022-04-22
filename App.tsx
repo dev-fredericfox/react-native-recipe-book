da1563a73d21a65999512d0c2ba8e44861b19724
Frederic Fox <dev.frederic.fox@gmail.com>
2022-04-22 16:38:53 +0200

    Add Privacy Link

--- App.tsx
+++ App.tsx
@@ -6,6 +6,8 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 import HomeScreen from "./screens/HomeScreen";
 import RecipeScreen from "./screens/RecipeScreen";
+import Imprint from "./screens/Imprint"
+import Privacy from "./screens/Privacy"
 import { RootStackParamList } from "./constants/Types";
 
 
@@ -17,6 +19,8 @@ export default function App() {
       <Stack.Navigator initialRouteName="Home">
         <Stack.Screen options={{headerTitle: 'Freds Cooking Corner'}} name="Home" component={HomeScreen} />
         <Stack.Screen name="Recipe" component={RecipeScreen} options={{headerBackTitleVisible: false}} />
+        <Stack.Screen name="Imprint" component={Imprint} options={{headerBackTitleVisible: false}} />
+        <Stack.Screen name="Privacy" component={Privacy} options={{headerBackTitleVisible: false}} />
       </Stack.Navigator>
     </NavigationContainer>
   );
